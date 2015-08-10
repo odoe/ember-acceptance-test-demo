fad82c69604089ca3522e43c34cb9fbd3ee60374
rene rubalcava <rrubalcava@esri.com>
2015-08-10 16:25:25 -0700

    add console log

--- app/services/fake.js
+++ app/services/fake.js
@@ -4,6 +4,7 @@ export default Ember.Service.extend({
   update: null,
   names: null,
   queryNames: Ember.observer('upper', function() {
+    console.log('fake service, set names');
     setTimeout(() => {
       this.set('names', ['tim', 'tom', 'bob', 'bill']);
     }, 500);
